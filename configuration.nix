bf615ea8a670620a041a99890766df6b35cca39c
Glen Ernstrom <discombobulatedpancake@proton.me>
2025-01-07 16:41:26 -0500

    added nix-ld

--- configuration.nix
+++ configuration.nix
@@ -214,9 +214,26 @@
   # Enable flatpak
   services.flatpak.enable = true;
 
-  #Install Murmur
+  # Install Murmur
     services.murmur = {
     enable = true;
     openFirewall = true;
   };
+
+  # Enable nix-ld
+  programs.nix-ld.enable = true;
+
+   # Sets up all the libraries to load
+  programs.nix-ld.libraries = with pkgs; [
+    stdenv.cc.cc
+    zlib
+    fuse3
+    icu
+    nss
+    openssl
+    curl
+    expat
+    # ...
+  ]; 
+
 }
