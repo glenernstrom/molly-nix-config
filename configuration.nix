f11f037bfaf3454de6968cbef52b4d13c3da14b8
Glen Ernstrom <discombobulatedpancake@proton.me>
2025-01-09 08:27:10 -0500

    removed murmur and removed an extra };

--- configuration.nix
+++ configuration.nix
@@ -214,23 +214,7 @@
   # Enable flatpak
   services.flatpak.enable = true;
 
-  };
-
   # Enable nix-ld
   programs.nix-ld.enable = true;
-
-   # Sets up all the libraries to load
-  programs.nix-ld.libraries = with pkgs; [
-    stdenv.cc.cc
-    zlib
-    fuse3
-    icu
-    nss
-    openssl
-    curl
-    expat
-    jre17_minimal
-    # ...
-  ]; 
 
 }
